2b21569bd7c441473a9a4947766bce19107c774c
mariuszchucherko <mariuszchucherko@workhuman.com>
2021-11-02 15:24:27 +0000

    After 55. Create the Spring Boot Project

---
+++ src/main/java/pl/com/mcsystems/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package pl.com.mcsystems.spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/mcsystems/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.mcsystems.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
